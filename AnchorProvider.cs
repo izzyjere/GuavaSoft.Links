b3b727eb3693a8b0ff9805e2750d69d9e57ce746
izzyjere <wisdomjere5@gmail.com>
2022-11-11 11:53:34 +0200

    Add project files.

---
+++ AnchorProvider.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuavaSoft.Links
+{
+    internal class AnchorProvider : ComponentBase, IDisposable
+    {
+        [Inject] public NavigationManager NavigationManager { get; set; }
+        [Inject] public IJSRuntime JSRuntime { get; set; }
+
+        [Parameter] public RenderFragment ChildContent { get; set; }
+        private Lazy<Task<IJSObjectReference>> moduleTask;
+        string Anchor { get; set; }
+
+        bool ForceScroll { get; set; }
+
+        protected override void OnInitialized()
+        {
+            moduleTask = new(() => JSRuntime.InvokeAsync<IJSObjectReference>(
+               "import", "./_content/GuavaSoft.Links/main.js").AsTask());
+            base.OnInitialized();
+
+            NavigationManager.LocationChanged += OnLocationChanged;
+        }
+
+        protected override Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+                ScrollToAnchor(forceScroll: true);
+
+            return base.OnAfterRenderAsync(firstRender);
+        }
+
+        void OnLocationChanged(object sender, LocationChangedEventArgs args)
+        {
+            ScrollToAnchor(NavigationManager.ToAbsoluteUri(args.Location).Fragment);
+        }
+
+        async void ScrollToAnchor(string anchor = "", bool forceScroll = false)
+        {
+            var module = await moduleTask.Value;
+            if (module != null)
+            {
+                if (!string.IsNullOrEmpty(anchor) || forceScroll)
+                   await module.InvokeAsync<string>("scrollToAnchor", anchor);
+            }
+        }
+
+        void IDisposable.Dispose()
+        {
+            NavigationManager.LocationChanged -= OnLocationChanged;
+        }
+    }
+}
---
+++ wwwroot/main.js
@@ -0,0 +1,13 @@
+function scrollToAnchor(anchor) {
+    var selector = anchor || document.location.hash;
+    if (selector && selector.length > 1) {
+        var element = document.querySelector(selector);
+        if (element) {
+            var y = element.getBoundingClientRect().top + window.pageYOffset;         
+            y -= document.querySelector(".main").offsetHeight;
+            window.scroll(0, y);
+        }
+    }
+    else
+        window.scroll(0, 0);
+}
